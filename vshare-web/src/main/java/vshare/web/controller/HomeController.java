cb846fafe8e64875583b414caa6d5a836b5b042d
root <iuem@0Thaydoi>
2017-10-17 13:21:05 +0700

    refactoring, more readable

--- vshare-web/src/main/java/vshare/web/controller/HomeController.java
+++ vshare-web/src/main/java/vshare/web/controller/HomeController.java
@@ -1,46 +1,15 @@
 package vshare.web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import vshare.common.binding.UploadFileInfo;
-import vshare.common.repository.FileRepository;
-import vshare.common.repository.FolderRepository;
-import vshare.common.repository.StorageRepository;
-import vshare.common.repository.UserRepository;
-import vshare.common.service.FileManager;
-import vshare.common.service.FolderManager;
-import vshare.common.service.StorageManager;
 
 @Controller
 @RequestMapping(path = "home")
 public class HomeController extends BaseController {
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    StorageRepository storageRepository;
-
-    @Autowired
-    FolderRepository folderRepository;
-
-    @Autowired
-    FolderController folderController;
-
-    @Autowired
-    StorageManager storageManager;
-
-    @Autowired
-    FileRepository fileRepository;
-
-    @Autowired
-    FileManager fileManager;
-
-    @Autowired
-    FolderManager folderManager;
 
     @GetMapping
     String getHomeUI(Model model) {
@@ -55,8 +24,8 @@ public class HomeController extends BaseController {
         return "home";
     }
 
-    @GetMapping(value = "/{child}")
-    String getChildFolder(Model model, @PathVariable("child") Long curFolderId) {
+    @GetMapping(value = "/{folderId}")
+    String getChildFolder(Model model, @PathVariable("folderId") Long curFolderId) {
 
         model.addAttribute("folders", getFolders(curFolderId));
         model.addAttribute("files", getFiles(curFolderId));
