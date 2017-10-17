6c34b11a0875dd8fe979c739bc5a89042b18f8de
root <iuem@0Thaydoi>
2017-10-17 15:11:33 +0700

    refactoring: remove unused

--- vshare-web/src/main/java/vshare/web/controller/FolderController.java
+++ vshare-web/src/main/java/vshare/web/controller/FolderController.java
@@ -1,50 +1,20 @@
 package vshare.web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import vshare.common.binding.NewFolder;
 import vshare.common.entity.FolderEntity;
-import vshare.common.repository.FolderRepository;
-import vshare.common.service.FolderManager;
-import vshare.common.service.StorageManager;
-
-import java.util.List;
 
 @Controller
 @RequestMapping(path = "folders")
 public class FolderController extends BaseController {
-
-    @Autowired
-    FolderManager folderManager;
-
-    @Autowired
-    FolderRepository folderRepository;
-
-    @Autowired
-    StorageManager storageManager;
-
-    @GetMapping
-    @ResponseBody
-    public List<FolderEntity> getFolderList() {
-        return null;
-    }
-
     @PostMapping
     @ResponseBody
-    public String createFolder(@ModelAttribute("newFolder") NewFolder newFolder) {
+    public FolderEntity createFolder(@ModelAttribute("newFolder") NewFolder newFolder) {
         FolderEntity folderEntity = createFolder(newFolder.getParrentId(), newFolder.getName());
-        return "okay";
+        return folderEntity;
     }
-    /*@PostMapping
-    @ResponseBody
-    public String createFolder(@RequestParam("parrentId") Long parrentId, @RequestParam("folderName") String folderName) {
-        FolderEntity folderEntity = new FolderEntity();
-        folderEntity.setFolderParent(parrentId);
-        folderEntity.setFolderName(folderName);
-        long storageId = storageManager.getLoggedInStorageId();
-        folderEntity.setStorageId(storageId);
-        folderRepository.save(folderEntity);
-        return "xinc";
-    }*/
 }
